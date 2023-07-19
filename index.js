9944c9769aabc08be2bf7e0677c92502001575a2
popaul <skydaz73170@gmail.com>
2023-07-19 14:30:56 +0200

    2iem version

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const img1 = document.getElementById("img1")
 const img2 = document.getElementById("img2")
 const img3 = document.getElementById("img3")
-
+const i2 = document.getElementById("i1")
+const i1 = document.getElementById("i2")
 const list_img = [img1,img2,img3]
+const cross = document.getElementById("cross")
+const box = document.getElementById('box')
+
+img2.addEventListener("click", ()=>{
+    window.open('https://www.youtube.com/watch?v=DJoCZmoB-aw','_blank')
+})
+
 
 
 function myFunction(x) {
@@ -21,7 +29,23 @@ function myFunction(x) {
                 })
             });
             }
-            
+            img1.addEventListener("click",()=>{
+    
+                i1.style.top="20vh"
+                i2.style.top="100vh"
+                box.style.display ="flex"
+                
+            })
+            img3.addEventListener("click",()=>{
+                
+                i2.style.top="20vh"
+                i1.style.top="100vh"
+                box.style.display ="flex"
+            })
+
+            cross.addEventListener("click", ()=>{
+                box.style.display="none"
+            })
             menu(list_img);
     } else {
         function menu (arg){
@@ -37,9 +61,24 @@ function myFunction(x) {
                     e.style.opacity="1"
                 })
             });
+            
             }
             
             menu(list_img);
+            
+img1.addEventListener("click",()=>{
+    
+    i1.style.top="0px"
+    i2.style.top="500px"
+    
+})
+img3.addEventListener("click",()=>{
+    
+    i2.style.top="0px"
+    i1.style.top="500px"
+   
+})
+
     }
   }
 
